1d1d51a6d24da84ffde25d9e6a5a99b4e8e1d349
aashi158 <aashibhargava158@gmail.com>
2023-02-24 16:16:58 +0530

    hello

--- database/connectdb.js
+++ database/connectdb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 con = "mongodb+srv://aashi123:aashi123@cluster0.tael0rh.mongodb.net/?retryWrites=true&w=majority"
 const connectdb =()=>{
     // return mongoose.connect('mongodb://localhost:27017/blog_website') 
-    return mongoose.connect(con)    // Database name blog_website
+  return mongoose.connect(con)  // Database name blog_website
     .then(()=>{
         console.log("Connection Sucessfully")
     })
